082aa8f8fde0e856b83e62a6992efde10c97cb1e
ledeDEV02 <vladimirlebedev00010@gmail.com>
2022-02-13 18:53:10 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+inputValue: string = '';
+todoTasks: string[] = []
+error: string = ""
+	logHello(){
+		console.log("Hello!")
+	}
+	onInput(value: string){
+		this.inputValue = value
+	}
+	onClick(){
+		if(this.inputValue){
+			this.todoTasks.push(this.inputValue)
+			this.inputValue = ''
+			this.error = ""
+		} else{
+			this.error = "Your todo cannot be empty =)"
+		}
+	}
+	onSubmit($event: any){
+		$event.preventDefault()
+	}
+	onDeleteEvent(task: string){
+		this.todoTasks = this.todoTasks.filter(item => item !== task)
+		// console.log(task)
+	}
 }
